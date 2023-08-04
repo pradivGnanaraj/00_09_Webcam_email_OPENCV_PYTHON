fd9ef2c0be14b1d4dbfb955444f7f459ae72fb49
pradivgnanaraj <tpradivgnanaraj@gmail.com>
2023-08-04 15:16:40 +0530

    Detect Motion in Camera

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import cv2
-import time
 
 video = cv2.VideoCapture(0)
 
+first_frame = None
 while True:
     check, frame = video.read()
-    cv2.imshow("My video", frame)
 
+    # converting to grayscale
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21,21), 0)
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+
+    # making the static video to black and frames to white
+    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    # removing some noise
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    cv2.imshow("My video", dil_frame)
+
+    # Creating contours
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # removing false positives
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
+
+    cv2.imshow("Vidoe", frame)
     key = cv2.waitKey(1)
 
     if key == ord("q"):
